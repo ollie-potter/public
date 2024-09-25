13d3748d7ae50d338893dcefede7a65861157eba
Ollie Potter <u7969847@anu.edu.au>
2024-09-25 13:45:48 +1000

    Javascript version functional!

--- colour charge visualization/colour-charge.js
+++ colour charge visualization/colour-charge.js
@@ -1,16 +1,13 @@
-var redQuarks = 0;
-var greenQuarks = 0;
-var blueQuarks = 0;
-var antiRedQuarks = 0;
-var antiGreenQuarks = 0;
-var antiBlueQuarks = 0;
+var redQuarks = 0; var antiRedQuarks = 0; var redTotal = redQuarks+antiGreenQuarks+antiBlueQuarks;
+var greenQuarks = 0; var antiGreenQuarks = 0; var greenTotal = greenQuarks+antiRedQuarks+antiBlueQuarks;
+var blueQuarks = 0; var antiBlueQuarks = 0; var blueTotal = blueQuarks+antiRedQuarks+antiGreenQuarks;
 var maximum = 0;
 var waitFor = '';
 var numKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 function scaleValues(value, old_max, old_min, new_max, new_min, inverse=false){
     if(maximum == 0){return 0}
-    if (inverse){return round((new_min + (new_max - new_min) * ((value - old_min) / (old_max - old_min))))}
+    //if (inverse){return round((new_min + (new_max - new_min) * ((value - old_min) / (old_max - old_min))))}
     else{return round(255-(new_min + (new_max - new_min) * ((value - old_min) / (old_max - old_min))))}
     // effectively the map() function in other languages
 
@@ -21,24 +18,56 @@ function setup(){
 }
 
 function draw(){
-    colour = [scaleValues(redQuarks+antiGreenQuarks+antiBlueQuarks, maximum, 0, 0, 255), scaleValues(greenQuarks+antiRedQuarks+antiBlueQuarks, maximum, 0, 0, 255), scaleValues(blueQuarks+antiRedQuarks+antiGreenQuarks, maximum, 0, 0, 255)]
+    print(redQuarks, antiGreenQuarks, antiBlueQuarks, redTotal)
+    redTotal = Number(redQuarks)+Number(antiGreenQuarks)+Number(antiBlueQuarks)
+    greenTotal = Number(greenQuarks)+Number(antiRedQuarks)+Number(antiBlueQuarks)
+    blueTotal = Number(blueQuarks)+Number(antiRedQuarks)+Number(antiGreenQuarks)
 
-    maximum = max(max(redQuarks+antiGreenQuarks+antiBlueQuarks, greenQuarks+antiRedQuarks+antiBlueQuarks), blueQuarks+antiGreenQuarks+antiRedQuarks)
+    maximum = max(max(redTotal, greenTotal), blueTotal)
+
+    colour = [scaleValues(redTotal, maximum, 0, 0, 255), scaleValues(greenTotal, maximum, 0, 0, 255), scaleValues(blueTotal, maximum, 0, 0, 255)]
+    //print('red: '+redTotal+'/'+maximum, 'green: '+greenTotal+'/'+maximum, 'blue: '+blueTotal+'/'+maximum, )
     background(colour[0], colour[1], colour[2])
+    textSize(40)
+    stroke(0)
+    strokeWeight(3)
 
-    fill(255, 0, 0)
-    circle(1*windowWidth/4, 1*windowHeight/6, 40)
-    //text()
+    xOffset = -11
+    yOffset = 13
+
+    fill(255, 0, 0); 
+    circle(1*windowWidth/4, 1*windowHeight/6, 80)
+    fill(0);
+    text(redQuarks.toString(), (1*windowWidth/4)+xOffset, (1*windowHeight/6)+yOffset)
+
     fill(0, 255, 0)
-    circle(2*windowWidth/4, 1*windowHeight/6, 40)
+    circle(2*windowWidth/4, 1*windowHeight/6, 80)
+    fill(0);
+    text(greenQuarks.toString(), (2*windowWidth/4)+xOffset, (1*windowHeight/6)+yOffset)
+
     fill(0, 0, 255)
-    circle(3*windowWidth/4, 1*windowHeight/6, 40)
+    circle(3*windowWidth/4, 1*windowHeight/6, 80)
+    fill(0);
+    text(blueQuarks.toString(), (3*windowWidth/4)+xOffset, (1*windowHeight/6)+yOffset)
+
     fill(0, 255, 255)
-    circle(1*windowWidth/4, 5*windowHeight/6, 40)
+    circle(1*windowWidth/4, 5*windowHeight/6, 80)
+    fill(0);
+    text(antiRedQuarks.toString(), (1*windowWidth/4)+xOffset, (5*windowHeight/6)+yOffset)
+
     fill(255, 0, 255)
-    circle(2*windowWidth/4, 5*windowHeight/6, 40)
+    circle(2*windowWidth/4, 5*windowHeight/6, 80)
+    fill(0);
+    text(antiGreenQuarks.toString(), (2*windowWidth/4)+xOffset, (5*windowHeight/6)+yOffset)
+
     fill(255, 255, 0)
-    circle(3*windowWidth/4, 5*windowHeight/6, 40)
+    circle(3*windowWidth/4, 5*windowHeight/6, 80)
+    fill(0);
+    text(antiBlueQuarks.toString(), (3*windowWidth/4)+xOffset, (5*windowHeight/6)+yOffset)
+
+    strokeWeight(2)
+    if(colour == [255, 255, 255].toString()){text('Colour charge is balanced!', -250+windowWidth/2, 25+windowHeight/2)} else{print(colour)}
+
     
 }
 
@@ -46,13 +75,16 @@ function keyTyped() {
 
     for(i in numKeys) {
         if (key == numKeys[i]) {
-            if (waitFor == 'r') {redQuarks = i.toString()}
-            if (waitFor == 'g') {greenQuarks = i.toString()}
-            if (waitFor == 'b') {blueQuarks = i.toString()}
-            if (waitFor == 'ar') {antiRedQuarks = i.toString()}
-            if (waitFor == 'ag') {antiGreenQuarks = i.toString()}
-            if (waitFor == 'ab') {antiBlueQuarks = i.toString()}
-            console.log('r:',redQuarks, 'g:',greenQuarks, 'b:',blueQuarks, 'ar:',antiRedQuarks, 'ag:',antiGreenQuarks, 'ab',antiBlueQuarks)
+
+            print(i)
+            if (waitFor == 'r') {redQuarks = i}
+            if (waitFor == 'g') {greenQuarks = i}
+            if (waitFor == 'b') {blueQuarks = i}
+            if (waitFor == 'ar') {antiRedQuarks = i}
+            if (waitFor == 'ag') {antiGreenQuarks = i}
+            if (waitFor == 'ab') {antiBlueQuarks = i}
+            waitFor = ''
+            //console.log('r:',redQuarks, 'g:',greenQuarks, 'b:',blueQuarks, 'ar:',antiRedQuarks, 'ag:',antiGreenQuarks, 'ab',antiBlueQuarks)
             break
         }
     }
